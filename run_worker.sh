aaf87fefbdd1546be4a57df4f3c4e76405034dec
Mingxun Wang <mwang87@gmail.com>
2020-03-28 11:48:35 -0700

    initial commit for qemistree

---
+++ run_worker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Trying to remove X1 lock
+rm /tmp/.X1-lock || true
+
+#Running Cytoscape Virtual Buffer
+Xvfb :1 &
+sleep 1
+export DISPLAY=:1
+
+celery -A worker_tasks worker -l info -c 1
