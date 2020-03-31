b32f249b9655fb29347e4b4fcb1b75432b44e853
Anupriya Tripathi <anupriya.tripathi92@gmail.com>
2020-03-31 15:21:47 -0700

    switch to CLI

--- dash-app/app.py
+++ dash-app/app.py
@@ -4,9 +4,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import q2_qemistree
-from q2_qemistree import prune_hierarchy
-from q2_qemistree import plot
-from qiime2 import Artifact
 import os
 
 import pandas as pd
@@ -100,9 +97,9 @@ def process_qemistree(qemistree_task, prune_col, plot_col,
 
     # Metadata File
     output_filename = './output/{}_metadata.tsv'.format(qemistree_task)
-    ftable = requests.get(url +  '&file=metadata_table/&block=main')
+    metadata = requests.get(url +  '&file=metadata_table/&block=main')
     with open(output_filename, 'wb') as output_file:
-        output_file.write(ftable.content)
+        output_file.write(metadata.content)
 
     # hashed feature table
     output_filename = './output/{}_merged-feature-table.qza'.format(qemistree_task)
@@ -115,37 +112,42 @@ def process_qemistree(qemistree_task, prune_col, plot_col,
     output_filename = './output/{}_qemistree.qza'.format(qemistree_task)
     tree = "&file=output_folder/qemistree.qza&block=main"
     tree = requests.get(url+tree)
-    with open(output_filename, "wb") as qza:
+    with open(output_filename, 'wb') as qza:
         qza.write(tree.content)
-    tree = Artifact.load(output_filename).view(TreeNode)
+    # tree = Artifact.load(output_filename).view(TreeNode)
 
     # feature data with classyfire taxonomy
     output_filename = './output/{}_classified-feature-data.qza'.format(qemistree_task)
     fdata = "&file=output_folder/classified-feature-data.qza&block=main"
     fdata = requests.get(url+fdata)
-    with open(output_filename, "wb") as qza:
+    with open(output_filename, 'wb') as qza:
         qza.write(fdata.content)
-    fdata = Artifact.load(output_filename).view(pd.DataFrame)
+    # fdata = Artifact.load(output_filename).view(pd.DataFrame)
 
-    pruned_tree = prune_hierarchy(fdata, tree, prune_col)
-    ntips = len([tip for tip in pruned_tree.tips()])
-
-    os.system("qiime feature-table group --i-table ./output/grouped-merged-feature-table.qza "
-              "--p-axis 'sample' --m-metadata-file ./output/metadata.tsv "
-              "--m-metadata-column group_samples_col --p-mode mean-ceiling "
-              "--o-grouped-table ./output/grouped-merged-feature-table.qza")
+    # tree pruning
+    prune_cmd = ("qiime qemistree prune-hierarchy --i-feature-data ./output/{}_classified-feature-data.qza "
+                 "--i-tree ./output/{}_qemistree.qza --p-column {} "
+                 "--o-pruned-tree ./output/{}_qemistree-pruned.qza"
+                ).format(qemistree_task, qemistree_task, prune_col, qemistree_task)
+    os.system(prune_cmd)
     
-    grouped_ftable = Artifact.load("./output/grouped-merged-feature-table.qza").view(pd.DataFrame)
-    plot(output_dir ='./output/visualization', tree = pruned_tree, 
-         feature_metadata = fdata,
-         category = plot_col, 
-         ms2_label = ms2_label,
-         color_palette = 'Dark2', 
-         parent_mz = parent_mz,
-         grouped_table = grouped_table,
-         normalize_features = normalize_features)
-
-    return 'The number of features after filtering qemistree: {}'.format(ntips)
+    # feature table grouping
+    group_cmd = ("qiime feature-table group --i-table ./output/{}_merged-feature-table.qza "
+                 "--p-axis 'sample' --m-metadata-file ./output/{}_metadata.tsv "
+                 "--m-metadata-column '{}' --p-mode 'mean-ceiling' "
+                 "--o-grouped-table ./output/{}_grouped-merged-feature-table.qza"
+                ).format(qemistree_task, qemistree_task, group_samples_col, qemistree_task)
+    os.system(group_cmd)
+    
+    # qemistree plotting
+    plot_cmd = ("qiime qemistree plot --i-tree ./output/{}_qemistree-pruned.qza "
+                "--i-feature-metadata ./output/{}_classified-feature-data.qza "
+                "--i-grouped-table ./output/{}_grouped-merged-feature-table.qza "
+                "--p-category '{}' --p-ms2-label {} --p-parent-mz {} --p-normalize-features {} "
+                "--o-visualization ./output/{}_qemistree.qzv"
+                ).format(qemistree_task, qemistree_task, qemistree_task, plot_col,  
+                         ms2_label, parent_mz, normalize_features, qemistree_task)
+    os.system(plot_cmd)   
 
 
 if __name__ == "__main__":
